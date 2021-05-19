7b2eac02ff5cda6fe2f03061b8a49b31061144e4
Markdekaiser <mark.manuel.business@gmail.com>
2021-05-20 02:11:05 +0800

    Getting input

--- src/tesseract/frontend/LoginController.java
+++ src/tesseract/frontend/LoginController.java
@@ -5,7 +5,10 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import tesseract.backend.userLogin;
 
 public class LoginController {
 
@@ -20,16 +23,26 @@ public class LoginController {
 
     @FXML
     private Button loginBtn;
+    
+    @FXML
+    private TextField emailAd;
+    
+    @FXML
+    private PasswordField pass;
 
     @FXML
     void clickedLoginBtn(ActionEvent event) {
-        
+        userLogin.check(this.getEmail());
     }
 
     @FXML
     void initialize() {
         assert mainPane != null : "fx:id=\"mainPane\" was not injected: check your FXML file 'login.fxml'.";
         assert loginBtn != null : "fx:id=\"loginBtn\" was not injected: check your FXML file 'login.fxml'.";
-
+    }
+    
+    public String getEmail(){
+        String email = emailAd.getText(); 
+        return email;
     }
 }
